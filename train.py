06363fe82e946312e72641b224b7abe705b7d602
Hen Halpert <henhhalpert@gmail.com>
2021-03-17 22:00:47 -0700

    adding an option to save model
    after model is trained, can now be saved

--- train.py
+++ train.py
@@ -1,29 +1,39 @@
-from model import model_ft
+from model import model_ft,save_model
 import argparse
+
+# if no arguments are given, arguments will default to specific values
+# data directory must be given
 
 parser = argparse.ArgumentParser(description = "testing")
 parser.add_argument('data_directory', type=str)
-parser.add_argument('--save_dir', type=str)
-parser.add_argument('--arch', type=str)
-parser.add_argument('--learning_rate', type=float)
-parser.add_argument('--epochs', type=int)
-parser.add_argument('--hidden_units', type=int, default=1)
+parser.add_argument('--save_dir', nargs='?', const=1, type=str, default='checkpoint.pth')
+parser.add_argument('--arch', nargs='?', const=1, type=str, default='vgg13')
+parser.add_argument('--learning_rate', nargs='?', const=1, type=float, default=0.001)
+parser.add_argument('--epochs', nargs='?', const=1, type=int, default=1)
+parser.add_argument('--hidden_units', nargs='?', const=1, type=int, default=512)
 parser.add_argument('--gpu',action='store_true')
 args = parser.parse_args()
-# train_parser.add_argument('--processor', action="store", dest="processor", default="GPU")
-# print("Image Directory: ", args.data_directory)
 
 if args.gpu:
     args.gpu = 'cuda'
 else:
     args.gpu = 'cpu'
-print("cpu: ", args.gpu, "data_dir:", args.data_directory, "save_dir:", args.save_dir)
+print("--------------")
+print("data_directory: ", args.data_directory,
+      "save_dir:", args.save_dir,
+      "arch:", args.arch,
+      "learning_rate:", args.learning_rate,
+      "epochs:", args.epochs,
+      "hidden_units:", args.hidden_units,
+      "gpu:", args.gpu
+     )
 
+model = model_ft(args.data_directory, save_dir=args.save_dir,
+                 architecture=args.arch, lr=args.learning_rate,
+                 hidden_units=args.hidden_units, epochs=args.epochs, device=args.gpu)
 
-# model = model_ft(args.data_directory, save_dir=args.save_dir,
-#                  architecture=args.arch, lr=args.learning_rate,
-#                  hidden_units=args.hidden_units, epochs=args.epochs, device=args.gpu)
-
-
-
-
+if args.save_dir:
+    save_model(model, args.save_dir)
+else:
+    print("save_dir not specified")
+          
